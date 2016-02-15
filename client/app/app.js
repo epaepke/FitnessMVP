24457ace89f3ef1e4aa77e9ceb6c81cfe9e3846b
Erick Paepke <epaepke@gmail.com>
2016-02-15 12:23:11 -0800

    Route users to signin page

---
+++ client/app/app.js
@@ -0,0 +1,27 @@
+console.log('loaded app');
+
+angular.module('fitness', ['fitness.auth',
+  'ngRoute'
+])
+.config(function ($routeProvider, $httpProvider) {
+  console.log('should route from here');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .when('/signin', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .otherwise({
+      redirectTo: 'http://www.google.com'
+    });
+    // We add our $httpInterceptor into the array
+    // of interceptors. Think of it like middleware for your ajax calls
+    // $httpProvider.interceptors.push('AttachTokens');
+})
